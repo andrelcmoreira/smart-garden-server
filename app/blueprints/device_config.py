d859a8dfd0db97429f66b91523e97f4661d30ddd
André L. C. Moreira <andrelcmoreira@disroot.org>
2024-01-14 14:58:07 -0300

    adapting the POST /devices/<id>/config endpoint to use a mysql database

--- app/blueprints/device_config.py
+++ app/blueprints/device_config.py
@@ -4,9 +4,11 @@ from flask_jwt_extended import jwt_required
 
 from storage.models.config import Config
 from validators import validate_request, validate_field
-from . import CONFIGS_DB, DEVICES_DB
+
+from . import db
 
 device_cfg_bp = Blueprint('device_config', __name__, url_prefix='/devices')
+
 
 @device_cfg_bp.route('/<string:dev_id>/config/', methods=['POST'])
 @jwt_required()
@@ -27,10 +29,6 @@ def config_device(dev_id):
         (not validate_request(request.json)):
         abort(400, 'Bad request')
 
-    if not DEVICES_DB.get(dev_id):
-        app.logger.debug(f'device {dev_id} not found on database')
-        abort(404, 'Device not registered on database')
-
     try:
         # mandatory parameters
         interval = request.json['interval']
@@ -38,102 +36,115 @@ def config_device(dev_id):
         app.logger.debug(f'missing {key} in request')
         abort(400, 'Missing required data')
 
-    cfg = Config(id=dev_id, group=request.json.get('group'), interval=interval)
+    group = request.json.get('group') # optional
 
-    CONFIGS_DB.add(cfg)
+    with db.cursor() as cursor:
+        cursor.execute(f'select * from devices where id = {dev_id}')
+
+        ret = cursor.fetchone()
+        if not ret:
+            app.logger.debug(f'device {dev_id} not found')
+            abort(404, "The device isn't registered on database")
+
+        app.logger.debug(f'found device -> {ret}')
+
+        cursor.execute(f'insert into configs(dev_id, dev_group, dev_interval) \
+                       values ("{dev_id}", "{group}", "{interval}")')
+        db.commit()
 
     return jsonify({ 'msg': 'Configuration registered with success' }), 201
 
-@device_cfg_bp.route('/<string:dev_id>/config/', methods=['GET'])
-@jwt_required()
-def get_config(dev_id):
-    '''
-    GET /devices/<id>/config endpoint implementation.
 
-    :dev_id: ID of device.
-
-    :returns: On success, the device configuration; otherwise the suitable error
-              reply (see the API documentation for more informations).
-
-    '''
-    if (not validate_field('id', dev_id)):
-        abort(400, 'Bad request')
-
-    cfg = CONFIGS_DB.get(dev_id)
-    if not cfg:
-        abort(404, "The config isn't registered on database")
-
-    return jsonify(cfg), 200
-
-@device_cfg_bp.route('/config/', methods=['GET'])
-@jwt_required()
-def get_configs():
-    '''
-    GET /devices/config endpoint implementation.
-
-    :returns: On success, all the device configurations; otherwise an empty
-              reply.
-
-    '''
-    configs = CONFIGS_DB.get_all()
-
-    return jsonify(configs), 200
-
-@device_cfg_bp.route('/<string:dev_id>/config/', methods=['DELETE'])
-@jwt_required()
-def del_config(dev_id):
-    '''
-    DELETE /devices/<id>/config endpoint implementation.
-
-    :dev_id: ID of device.
-
-    :returns: On success, a success reply; otherwise the suitable error reply
-              (see the API documentation for more informations).
-
-    '''
-    app.logger.debug(f'device id -> {dev_id}')
-
-    if not validate_field('id', dev_id):
-        abort(400, 'Bad request')
-
-    if not CONFIGS_DB.get(dev_id):
-        app.logger.debug(f'config for device {dev_id} not found')
-        abort(404, "There's no config for the specific device")
-
-    CONFIGS_DB.rm(dev_id)
-
-    return jsonify({ 'msg': 'Config deleted from database' }), 200
-
-@device_cfg_bp.route('/<string:dev_id>/config/', methods=['PUT'])
-@jwt_required()
-def update_config(dev_id):
-    '''
-    PUT /devices/<id>/config endpoint implementation.
-
-    :dev_id: ID of device.
-
-    :returns: On success, a success reply; otherwise the suitable error reply
-              (see the API documentation for more informations).
-
-    '''
-    app.logger.debug(f'device id -> {dev_id}')
-
-    if (not validate_field('id', dev_id)) or \
-        (not validate_request(request.json)):
-        abort(400, 'Bad request')
-
-    try:
-        # mandatory parameters
-        param = request.json['param']
-        val = request.json['value']
-    except KeyError as key:
-        app.logger.debug(f'missing {key} in request')
-        abort(400, 'Missing required data')
-
-    if not CONFIGS_DB.get(dev_id):
-        app.logger.debug(f'config for device {dev_id} not found')
-        abort(404, "There's no config for the specific device")
-
-    CONFIGS_DB.update(dev_id, param, val)
-
-    return jsonify({ 'msg': 'Config updated in database' }), 200
+#@device_cfg_bp.route('/<string:dev_id>/config/', methods=['GET'])
+#@jwt_required()
+#def get_config(dev_id):
+#    '''
+#    GET /devices/<id>/config endpoint implementation.
+#
+#    :dev_id: ID of device.
+#
+#    :returns: On success, the device configuration; otherwise the suitable error
+#              reply (see the API documentation for more informations).
+#
+#    '''
+#    if (not validate_field('id', dev_id)):
+#        abort(400, 'Bad request')
+#
+#    cfg = CONFIGS_DB.get(dev_id)
+#    if not cfg:
+#        abort(404, "The config isn't registered on database")
+#
+#    return jsonify(cfg), 200
+#
+#@device_cfg_bp.route('/config/', methods=['GET'])
+#@jwt_required()
+#def get_configs():
+#    '''
+#    GET /devices/config endpoint implementation.
+#
+#    :returns: On success, all the device configurations; otherwise an empty
+#              reply.
+#
+#    '''
+#    configs = CONFIGS_DB.get_all()
+#
+#    return jsonify(configs), 200
+#
+#@device_cfg_bp.route('/<string:dev_id>/config/', methods=['DELETE'])
+#@jwt_required()
+#def del_config(dev_id):
+#    '''
+#    DELETE /devices/<id>/config endpoint implementation.
+#
+#    :dev_id: ID of device.
+#
+#    :returns: On success, a success reply; otherwise the suitable error reply
+#              (see the API documentation for more informations).
+#
+#    '''
+#    app.logger.debug(f'device id -> {dev_id}')
+#
+#    if not validate_field('id', dev_id):
+#        abort(400, 'Bad request')
+#
+#    if not CONFIGS_DB.get(dev_id):
+#        app.logger.debug(f'config for device {dev_id} not found')
+#        abort(404, "There's no config for the specific device")
+#
+#    CONFIGS_DB.rm(dev_id)
+#
+#    return jsonify({ 'msg': 'Config deleted from database' }), 200
+#
+#@device_cfg_bp.route('/<string:dev_id>/config/', methods=['PUT'])
+#@jwt_required()
+#def update_config(dev_id):
+#    '''
+#    PUT /devices/<id>/config endpoint implementation.
+#
+#    :dev_id: ID of device.
+#
+#    :returns: On success, a success reply; otherwise the suitable error reply
+#              (see the API documentation for more informations).
+#
+#    '''
+#    app.logger.debug(f'device id -> {dev_id}')
+#
+#    if (not validate_field('id', dev_id)) or \
+#        (not validate_request(request.json)):
+#        abort(400, 'Bad request')
+#
+#    try:
+#        # mandatory parameters
+#        param = request.json['param']
+#        val = request.json['value']
+#    except KeyError as key:
+#        app.logger.debug(f'missing {key} in request')
+#        abort(400, 'Missing required data')
+#
+#    if not CONFIGS_DB.get(dev_id):
+#        app.logger.debug(f'config for device {dev_id} not found')
+#        abort(404, "There's no config for the specific device")
+#
+#    CONFIGS_DB.update(dev_id, param, val)
+#
+#    return jsonify({ 'msg': 'Config updated in database' }), 200
